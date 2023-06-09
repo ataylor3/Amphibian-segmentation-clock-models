01314b5fcafe64d4897263f7d231ae937306e26c
ataylor3 <85068163+ataylor3@users.noreply.github.com>
2023-06-09 14:47:55 -0600

    Update history.m

--- history.m
+++ history.m
@@ -1,6 +1,7 @@
-% function to define solution history
+% function to define "solution history" or initial conditions
 function v = history(t)
 
-v = [t+ 100; t + 100];
+v = [t+ 100; t + 100]; % arbitrarily set initial mRNA and protein molecule amounts to 100 (both)
+% i.e. when t = 0, mRNA = 100 and protein = 100
 
 end
