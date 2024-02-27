e755d6301f7f62726af005d6d4c17cc98cea0cf1
ataylor3 <85068163+ataylor3@users.noreply.github.com>
2024-02-27 12:29:04 -0700

    Update amplitude.m
    Updated intron splicing delay (Tin) to reflect updated species-specific clock period
    Cut code corresponding to a figure that no longer appears in supplemental material

--- Scripts_localFunctions/amplitude.m
+++ Scripts_localFunctions/amplitude.m
@@ -1,4 +1,4 @@
-%% FIGURES S6 and S7
+%% FIGURE S6
 clear 
 clc
 
@@ -10,11 +10,11 @@ k = 33; % mRNA synthesis rate in absence of inhibition
 p_crit = 420; % critical protein threshold (species-specific)
 
 Ttx = 6.89; % transcriptional delay
-Tin = 12.87; % intron splicing delay
+Tin = 12.78; % intron splicing delay
 Tp = 2.18; % translation delay
 
 
-%% S6A and S7A: A. mexicanum, Brownian Motion (normal diffusion) model
+%% S6A: A. mexicanum, Brownian Motion (normal diffusion) model
 % mRNA half-life = export time
 
 Texp = 11.97; % export delay (BM simulations, radius = 5.5)
@@ -40,7 +40,6 @@ for i = 1:20
 
         P_mRNA(i,j) = osc(1,1); % store period of gene expression
         A_mRNA(i,j) = osc(1,3); % store amplitude of mRNA expression
-        A_protein(i,j) = osc(1,4); % store amplitude of protein expression
 
         tot_delay = Tm + Tp;
         Total_delay(j,1) = tot_delay; % store total delay
@@ -70,21 +69,7 @@ ax.FontSize = 15;
 c.Label.String = 'Amplitude of mRNA expression (molecules)'
 c.Label.FontSize = 15;
 
-% plot amplitude of protein expression 
-A_protein(isinf(P_mRNA)) = 0; % set amplitude to 0 if oscillations are damped 
-
-figure()
-imagesc(Total_delay,HL_p_vector,A_protein)
-c = colorbar;
-title('{\it A. mexicanum} (BM model)', '{h_m = T_{exp}}','FontSize',18)
-xlabel('Total delay (min), {T_m + T_p}','FontSize',15)
-ylabel('Protein half-life (min), {h_p}','FontSize',15)
-ax = gca;
-ax.FontSize = 15;
-c.Label.String = 'Amplitude of protein expression (molecules)'
-c.Label.FontSize = 15;
-
-%% S6C and S7C: A. mexicanum, Brownian Motion (normal diffusion) model
+%% S6C: A. mexicanum, Brownian Motion (normal diffusion) model
 % mRNA half-life = 1/2 export time
 
 HL_m = Texp/2; % mRNA half life equal to 1/2 export delay
@@ -107,7 +92,6 @@ for i = 1:20
 
         P_mRNA(i,j) = osc(1,1);
         A_mRNA(i,j) = osc(1,3);
-        A_protein(i,j) = osc(1,4);
 
         tot_delay = Tm + Tp;
         Total_delay(j,1) = tot_delay;
@@ -138,22 +122,7 @@ ax.FontSize = 15;
 c.Label.String = 'Amplitude of mRNA expression (molecules)'
 c.Label.FontSize = 15;
 
-% plot amplitude of protein expression 
-A_protein(isinf(P_mRNA)) = 0; % set amplitude to 0 if oscillations are damped
-
-figure()
-imagesc(Total_delay,HL_p_vector,A_protein)
-c = colorbar;
-title('{\it A. mexicanum} (BM model)', '{h_m = 1/2 T_{exp}}','FontSize',18)
-xlabel('Total delay (min), {T_m + T_p}','FontSize',15)
-ylabel('Protein half-life (min), {h_p}','FontSize',15)
-ax = gca;
-ax.FontSize = 15;
-c.Label.String = 'Amplitude of protein expression (molecules)'
-c.Label.FontSize = 15;
-
-
-%% S6E and S7E: A. mexicanum, Brownian Motion (normal diffusion) model
+%% S6E: A. mexicanum, Brownian Motion (normal diffusion) model
 % mRNA half-life = 1/4 export time
 
 HL_m = Texp/4; % mRNA half life equal to 1/4 export delay
@@ -176,7 +145,6 @@ for i = 1:20
 
         P_mRNA(i,j) = osc(1,1);
         A_mRNA(i,j) = osc(1,3);
-        A_protein(i,j) = osc(1,4);
 
         tot_delay = Tm + Tp;
         Total_delay(j,1) = tot_delay;
@@ -207,21 +175,7 @@ ax.FontSize = 15;
 c.Label.String = 'Amplitude of mRNA expression (molecules)'
 c.Label.FontSize = 15;
 
-% plot amplitude of protein expression 
-A_protein(isinf(P_mRNA)) = 0; % set amplitude to 0 if oscillations are damped
-
-figure()
-imagesc(Total_delay,HL_p_vector,A_protein)
-c = colorbar;
-title('{\it A. mexicanum} (BM model)', '{h_m = 1/4 T_{exp}}','FontSize',18)
-xlabel('Total delay (min), {T_m + T_p}','FontSize',15)
-ylabel('Protein half-life (min), {h_p}','FontSize',15)
-ax = gca;
-ax.FontSize = 15;
-c.Label.String = 'Amplitude of protein expression (molecules)'
-c.Label.FontSize = 15;
-
-%% S6B and S7B: A. mexicanum, fractional Brownian Motion (obstructed diffusion) model
+%% S6B: A. mexicanum, fractional Brownian Motion (obstructed diffusion) model
 % mRNA half-life = export time
 
 Texp = 26.27; % export delay (fBM simulations, radius = 5.5)
@@ -248,7 +202,6 @@ for i = 1:20
 
         P_mRNA(i,j) = osc(1,1);
         A_mRNA(i,j) = osc(1,3);
-        A_protein(i,j) = osc(1,4);
 
         tot_delay = Tm + Tp;
         Total_delay(j,1) = tot_delay;
@@ -279,22 +232,7 @@ ax.FontSize = 15;
 c.Label.String = 'Amplitude of mRNA expression (molecules)'
 c.Label.FontSize = 15;
 
-
-% plot amplitude of protein expression 
-A_protein(isinf(P_mRNA)) = 0; % set amplitude to 0 if oscillations are damped
-
-figure()
-imagesc(Total_delay,HL_p_vector,A_protein)
-c = colorbar;
-title('{\it A. mexicanum} (fBM model)', '{h_m = T_{exp}}','FontSize',18)
-xlabel('Total delay (min), {T_m + T_p}','FontSize',15)
-ylabel('Protein half-life (min), {h_p}','FontSize',15)
-ax = gca;
-ax.FontSize = 15;
-c.Label.String = 'Amplitude of protein expression (molecules)'
-c.Label.FontSize = 15;
-
-%% S6D and S7D: A. mexicanum, fractional Brownian Motion (obstructed diffusion) model
+%% S6D: A. mexicanum, fractional Brownian Motion (obstructed diffusion) model
 % mRNA half-life = 1/2 export time
 
 HL_m = Texp/2; % mRNA half-life equal to 1/2 export delay
@@ -317,7 +255,6 @@ for i = 1:20
 
         P_mRNA(i,j) = osc(1,1);
         A_mRNA(i,j) = osc(1,3);
-        A_protein(i,j) = osc(1,4);
 
         tot_delay = Tm + Tp;
         Total_delay(j,1) = tot_delay;
@@ -348,21 +285,7 @@ ax.FontSize = 15;
 c.Label.String = 'Amplitude of mRNA expression (molecules)'
 c.Label.FontSize = 15;
 
-% plot amplitude of protein expression 
-A_protein(isinf(P_mRNA)) = 0; % set amplitude to 0 if oscillations are damped
-
-figure()
-imagesc(Total_delay,HL_p_vector,A_protein)
-c = colorbar;
-title('{\it A. mexicanum} (fBM model)', '{h_m = 1/2 T_{exp}}','FontSize',18)
-xlabel('Total delay (min), {T_m + T_p}','FontSize',15)
-ylabel('Protein half-life (min), {h_p}','FontSize',15)
-ax = gca;
-ax.FontSize = 15;
-c.Label.String = 'Amplitude of protein expression (molecules)'
-c.Label.FontSize = 15;
-
-%% S6F and S7F: A. mexicanum, fractional Brownian Motion (obstructed diffusion) model
+%% S6F: A. mexicanum, fractional Brownian Motion (obstructed diffusion) model
 % mRNA half-life = 1/4 export time
 
 HL_m = Texp/4; % mRNA half-life equal to 1/4 export delay
@@ -387,7 +310,6 @@ for i = 1:20
 
         P_mRNA(i,j) = osc(1,1);
         A_mRNA(i,j) = osc(1,3);
-        A_protein(i,j) = osc(1,4);
 
         tot_delay = Tm + Tp;
         Total_delay(j,1) = tot_delay;
@@ -416,18 +338,4 @@ ylabel('Protein half-life (min), {h_p}','FontSize',15)
 ax = gca;
 ax.FontSize = 15;
 c.Label.String = 'Amplitude of mRNA expression (molecules)'
-c.Label.FontSize = 15;
-
-% plot amplitude of protein expression 
-A_protein(isinf(P_mRNA)) = 0; % set amplitude to 0 if oscillations are damped
-
-figure()
-imagesc(Total_delay,HL_p_vector,A_protein)
-c = colorbar;
-title('{\it A. mexicanum} (fBM model)', '{h_m = 1/4 T_{exp}}','FontSize',18)
-xlabel('Total delay (min), {T_m + T_p}','FontSize',15)
-ylabel('Protein half-life (min), {h_p}','FontSize',15)
-ax = gca;
-ax.FontSize = 15;
-c.Label.String = 'Amplitude of protein expression (molecules)'
 c.Label.FontSize = 15;
